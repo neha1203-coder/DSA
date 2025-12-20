dc65f7fae02f59129de787a77d574e8b55e7038d
neha1203-coder <nehaskumari1203@gmail.com>
2025-12-20 20:15:00 +0530

    initial commit

--- array/bubble_sort.cpp
+++ array/bubble_sort.cpp
@@ -1,32 +1,32 @@
 //time complexity of bubble sort is O(n^2)
 #include <iostream>
 using namespace std;
-// void bubblesort(int arr[], int n){
-//     for(int i=0; i<n-1; i++){
-//         for(int j=0; j<n-i-1; j++){
-//             if(arr[j]>arr[j+1]){
-//                 swap(arr[j], arr[j+1]);
-//             }
-//         }
-//     }
-
-// }
 void bubblesort(int arr[], int n){
     for(int i=0; i<n-1; i++){
-        bool isswap=false;
-
         for(int j=0; j<n-i-1; j++){
             if(arr[j]>arr[j+1]){
                 swap(arr[j], arr[j+1]);
-                isswap=true;
             }
-        }
-        if(!isswap){
-            return;
         }
     }
 
 }
+// void bubblesort(int arr[], int n){
+//     for(int i=0; i<n-1; i++){
+//         bool isswap=false;
+
+//         for(int j=0; j<n-i-1; j++){
+//             if(arr[j]>arr[j+1]){
+//                 swap(arr[j], arr[j+1]);
+//                 isswap=true;
+//             }
+//         }
+//         if(!isswap){
+//             return;
+//         }
+//     }
+
+// }
 void printArray(int arr[], int n){
     for(int i=0; i<n; i++){
         cout<<arr[i]<<" ";
