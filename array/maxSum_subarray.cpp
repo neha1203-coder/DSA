c2e5e0a0e6e928bf1c9597040c46b1b4d7afdab7
neha1203-coder <nehaskumari1203@gmail.com>
2025-12-22 17:57:02 +0530

    initial commit

--- array/maxSum_subarray.cpp
+++ array/maxSum_subarray.cpp
@@ -5,22 +5,22 @@ int main(){
     int arr[] ={3,-5,3,5,-1,9,-8};
     int currsum;
     int maxSum=arr[0];
-    // for(int st=0; st<n; st++){
-    //     currsum=0;
-    //     for(int end=st; end<n; end++){
-    //         currsum+=arr[end];
-    //         maxSum=max(currsum, maxSum);
+    for(int st=0; st<n; st++){
+        currsum=0;
+        for(int end=st; end<n; end++){
+            currsum+=arr[end];
+            maxSum=max(currsum, maxSum);
             
-    //         }
+            }
 
-    //     }
-    for(int i=0; i<n; i++){
-        currsum+=arr[i];
-        maxSum = max(maxSum, currsum);
-        if(currsum<0)
-            currsum = 0;
+        }
+    // for(int i=0; i<n; i++){
+    //     currsum+=arr[i];
+    //     maxSum = max(maxSum, currsum);
+    //     if(currsum<0)
+    //         currsum = 0;
         
-    }
+    // }
         
     
     cout<<"maximum subarray sum:"<<maxSum<<endl;
