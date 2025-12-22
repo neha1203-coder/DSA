03ec5ad455e6842024a0ef65f4107070f9b3e4ce
neha1203-coder <nehaskumari1203@gmail.com>
2025-12-22 18:02:20 +0530

    initial commit

--- array/power.cpp
+++ array/power.cpp
@@ -15,36 +15,36 @@ using namespace std;
 // }
 
 
-double power(double x, int n) {
-    if (n == 0)
-        return 1;
+// double power(double x, int n) {
+//     if (n == 0)
+//         return 1;
 
-    return x * power(x, n - 1);
-}
-
-int main() {
-    cout << power(2, 5);
-    return 0;
-}
-
-
-// double power(double x, int n){
-//     double result = 1;
-//     long long exp = n;
-//     if(exp < 0){
-//         x = 1/x;
-//         exp = -exp;
-//     }
-//     while(exp>0){
-//         if(exp%2==1)
-//         result *= x;
-//         x *= x;
-//         exp /= 2;
-//     }
-//     return result;
+//     return x * power(x, n - 1);
 // }
-// int main(){
-//     cout << power(2,5);
+
+// int main() {
+//     cout << power(2, 5);
 //     return 0;
 // }
+
+
+double power(double x, int n){
+    double result = 1;
+    long long exp = n;
+    if(exp < 0){
+        x = 1/x;
+        exp = -exp;
+    }
+    while(exp>0){
+        if(exp%2==1)
+        result *= x;
+        x *= x;
+        exp /= 2;
+    }
+    return result;
+}
+int main(){
+    cout << power(2,5);
+    return 0;
+}
 
