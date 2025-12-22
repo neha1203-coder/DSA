977f2bdcea78d004e21c787c935ca3ff0dc8ab39
neha1203-coder <nehaskumari1203@gmail.com>
2025-12-22 17:59:32 +0530

    initial commit

--- array/power.cpp
+++ array/power.cpp
@@ -1,31 +1,31 @@
 #include <iostream>
 using namespace std;
-double power(double x, int n){
-    double result = 1;
-    for(int i=1; i<=n; i++){
-        result *= x;
-    }
-    return result;
-}
-int main(){
-    double x=2;
-    int n=5;
-    cout<<power(x, n);
-    return 0;
-}
-
-
-// double power(double x, int n) {
-//     if (n == 0)
-//         return 1;
-
-//     return x * power(x, n - 1);
+// double power(double x, int n){
+//     double result = 1;
+//     for(int i=1; i<=n; i++){
+//         result *= x;
+//     }
+//     return result;
 // }
-
-// int main() {
-//     cout << power(2, 5);
+// int main(){
+//     double x=2;
+//     int n=5;
+//     cout<<power(x, n);
 //     return 0;
 // }
+
+
+double power(double x, int n) {
+    if (n == 0)
+        return 1;
+
+    return x * power(x, n - 1);
+}
+
+int main() {
+    cout << power(2, 5);
+    return 0;
+}
 
 
 // double power(double x, int n){
