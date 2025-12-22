01c0298c2b3f3405ef834a58526b9c4d79633c3a
neha1203-coder <nehaskumari1203@gmail.com>
2025-12-22 17:58:48 +0530

    initial commit

--- array/power.cpp
+++ array/power.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
 using namespace std;
-// double power(double x, int n){
-//     double result = 1;
-//     for(int i=1; i<=n; i++){
-//         result *= x;
-//     }
-//     return result;
-// }
-// int main(){
-//     double x=2;
-//     int n=5;
-//     cout<<power(x, n);
-//     return 0;
-// }
+double power(double x, int n){
+    double result = 1;
+    for(int i=1; i<=n; i++){
+        result *= x;
+    }
+    return result;
+}
+int main(){
+    double x=2;
+    int n=5;
+    cout<<power(x, n);
+    return 0;
+}
 
 
 // double power(double x, int n) {
@@ -27,23 +27,24 @@ using namespace std;
 //     return 0;
 // }
 
-double power(double x, int n){
-    double result = 1;
-    long long exp = n;
-    if(exp < 0){
-        x = 1/x;
-        exp = -exp;
-    }
-    while(exp>0){
-        if(exp%2==1)
-        result *= x;
-        x *= x;
-        exp /= 2;
-    }
-    return result;
-}
-int main(){
-    cout << power(2,5);
-    return 0;
-}
+
+// double power(double x, int n){
+//     double result = 1;
+//     long long exp = n;
+//     if(exp < 0){
+//         x = 1/x;
+//         exp = -exp;
+//     }
+//     while(exp>0){
+//         if(exp%2==1)
+//         result *= x;
+//         x *= x;
+//         exp /= 2;
+//     }
+//     return result;
+// }
+// int main(){
+//     cout << power(2,5);
+//     return 0;
+// }
 
